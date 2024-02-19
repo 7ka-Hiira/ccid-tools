76a13a05f57270b3e92ebd1b4e42ea913e41418f
Nanaka Hiira <hiira@hiira.dev>
2024-02-19 14:28:18 +0900

    use hex crate

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,6 @@ use alloy_primitives::{keccak256, Address};
 use coins_bip39::{mnemonic::Mnemonic, English, Wordlist};
 use k256::ecdsa::{SigningKey, VerifyingKey};
 use k256::elliptic_curve::sec1::ToEncodedPoint;
-use std::fmt::Write;
 use std::str::FromStr;
 
 const DEFAULT_DERIVATION_PATH: &str = "m/44'/60'/0'/0/0";
@@ -33,20 +32,6 @@ pub fn pubkey_to_addr(pubkey: &[u8]) -> Address {
     Address::from_slice(&keccak256(&pubkey.as_bytes()[1..])[12..])
 }
 
-pub fn hex_decode(hex: &str) -> Vec<u8> {
-    (0..hex.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&hex[i..i + 2], 16).unwrap())
-        .collect::<Vec<u8>>()
-}
-
-pub fn hex_encode(bytes: &[u8]) -> String {
-    bytes.iter().fold(String::new(), |mut output, b| {
-        let _ = write!(output, "{:02x}", b);
-        output
-    })
-}
-
 pub fn start_with_cc(text: &str) -> bool {
     text.to_uppercase().starts_with("CC")
 }
@@ -62,28 +47,28 @@ pub fn phrase_to_ccid_str(mnemonic: &str) -> String {
 
 pub fn phrase_to_privkey_str(mnemonic: &str) -> String {
     let mnemonic: Mnemonic<English> = Mnemonic::from_str(mnemonic).unwrap();
-    hex_encode(mnemonic_to_privkey(&mnemonic).to_bytes().as_ref())
+    hex::encode(mnemonic_to_privkey(&mnemonic).to_bytes())
 }
 
 pub fn phrase_to_pubkey_str(mnemonic: &str) -> String {
     let mnemonic: Mnemonic<English> = Mnemonic::from_str(mnemonic).unwrap();
     let privkey = mnemonic_to_privkey(&mnemonic);
-    hex_encode(&privkey_to_pubkey(privkey))
+    hex::encode(&privkey_to_pubkey(privkey))
 }
 
 pub fn privkey_to_ccid_str(privkey: &str) -> String {
-    let key = SigningKey::from_slice(&hex_decode(privkey)).unwrap();
+    let key = SigningKey::from_slice(&hex::decode(privkey).unwrap()).unwrap();
     let addr = privkey_to_addr(key);
     addr.to_string().replace("0x", "CC")
 }
 
 pub fn privkey_to_pubkey_str(privkey: &str) -> String {
-    let key = SigningKey::from_slice(&hex_decode(privkey)).unwrap();
-    hex_encode(&privkey_to_pubkey(key))
+    let key = SigningKey::from_slice(&hex::decode(privkey).unwrap()).unwrap();
+    hex::encode(&privkey_to_pubkey(key))
 }
 
 pub fn pubkey_to_ccid_str(pubkey: &str) -> String {
-    let addr = pubkey_to_addr(&hex_decode(pubkey));
+    let addr = pubkey_to_addr(&hex::decode(pubkey).unwrap());
     addr.to_string().replace("0x", "CC")
 }
 
